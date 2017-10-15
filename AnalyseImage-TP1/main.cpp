a8e290f8f96937ca6c1a5d59e81d51e466848b6a
Crymsius <romain.m.mars@gmail.com>
2017-10-15 13:51:11 +0200

    [feat] update to main.cpp

--- AnalyseImage-TP1/main.cpp
+++ AnalyseImage-TP1/main.cpp
@@ -13,10 +13,11 @@
 #include "opencv2/highgui/highgui.hpp"
 
 cv::Mat image, destinationX, destinationY, destinationNorme, destinationDirection, destinationNormeSeuil, destinationNormeGris, GrisChan[3], seuilHaut, seuilHyst;
-cv::Mat destMulti0, destMulti1, destMulti2, destMulti3, distMulti, dirMulti, dirColorMulti;
+cv::Mat destMulti0, destMulti1, destMulti2, destMulti3, distMulti, distMultiSeuil, dirMulti, dirColorMulti;
 cv::Mat grayMulti0, grayMulti1, grayMulti2, grayMulti3;
+cv::Mat MultiThin;
 const int seuil_slider_max = 100;
-int seuil_slider = 20;
+int seuil_slider = 100;
 float seuil;
 
 const int seuil_hyst_slider_max = 100;
@@ -24,12 +25,15 @@ int seuil_hyst_slider = 10;
 float seuil_hyst;
 
 void seuillage (cv::Mat& source, cv::Mat& destination, float& threshold) {
-    for (int j=0; j < source.size().width; ++j) {
-        for (int i=0; i < source.size().height; ++i) {
-            if (source.at<float>(i,j) < threshold*255)
+    for (int i=0; i < source.size().height; ++i) {
+        for (int j=0; j < source.size().width; ++j) {
+            if (source.at<float>(i,j) < threshold*255) {
                 destination.at<float>(i,j) = 0;
-            else
+//                destination.at<uchar>(i,j) = 0;
+            } else {
                 destination.at<float>(i,j) = 255;
+//                destination.at<uchar>(i,j) = 255;
+            }
         }
     }
 }
@@ -63,14 +67,90 @@ void seuil_hysteresis (cv::Mat& source, cv::Mat& intermediaire, cv::Mat& destina
     cv::imshow("haut", seuilHaut);
 }
 
+void affinageMultiDir (cv::Mat& source, cv::Mat& destination, cv::Mat& gradient, cv::Mat& direction) {
+    for (int i=1; i < gradient.size().height-1; ++i) {
+        for (int j=1; j < gradient.size().width-1; ++j) {
+            if (source.at<float>(i,j) == 255) {
+                double angle = direction.at<float>(i,j) + M_PI;
+                if (fmod(angle, M_PI) == 0) {
+                    if (gradient.at<float>(i,j) >= gradient.at<float>(i,j-1) && gradient.at<float>(i,j) >= gradient.at<float>(i,j+1))
+                        destination.at<float>(i,j) = 255;
+                    else
+                        destination.at<float>(i,j) = 0;
+                } else if ( fmod(angle, M_PI) == M_PI_4) {
+                    if (gradient.at<float>(i,j) >= gradient.at<float>(i-1,j-1) && gradient.at<float>(i,j) >= gradient.at<float>(i+1,j+1))
+                        destination.at<float>(i,j) = 255;
+                    else
+                        destination.at<float>(i,j) = 0;
+                } else if (fmod(angle, M_PI) == M_PI_2) {
+                    if (gradient.at<float>(i,j) >= gradient.at<float>(i-1,j) && gradient.at<float>(i,j) >= gradient.at<float>(i+1,j))
+                        destination.at<float>(i,j) = 255;
+                    else
+                        destination.at<float>(i,j) = 0;
+                } else if ( fmod(angle, M_PI) == 3 * (M_PI_4)) {
+                    if (gradient.at<float>(i,j) >= gradient.at<float>(i+1,j-1) && gradient.at<float>(i,j) >= gradient.at<float>(i-1,j+1))
+                        destination.at<float>(i,j) = 255;
+                    else
+                        destination.at<float>(i,j) = 0;
+                }
+            } else {
+                destination.at<float>(i,j) = 0;
+            }
+        }
+    }
+}
+
+void affinageBiDir(cv::Mat& source, cv::Mat& destination, cv::Mat& gradient, cv::Mat& direction) {
+    for (int i=1; i < gradient.size().height-1; ++i) {
+        for (int j=1; j < gradient.size().width-1; ++j) {
+            if (source.at<float>(i,j) == 255) {
+                double angle = direction.at<float>(i,j) + 180;
+                if (fmod(angle,180) < 22.5) {
+                    if (gradient.at<float>(i,j) >= gradient.at<float>(i,j-1) && gradient.at<float>(i,j) >= gradient.at<float>(i,j+1))
+                        destination.at<float>(i,j) = 255;
+                    else
+                        destination.at<float>(i,j) = 0;
+                } else if (fmod(angle,180) < 67.5) {
+                    if (gradient.at<float>(i,j) >= gradient.at<float>(i-1,j-1) && gradient.at<float>(i,j) >= gradient.at<float>(i+1,j+1))
+                        destination.at<float>(i,j) = 255;
+                    else
+                        destination.at<float>(i,j) = 0;
+                } else if (fmod(angle,180) < 112.5) {
+                    if (gradient.at<float>(i,j) >= gradient.at<float>(i-1,j) && gradient.at<float>(i,j) >= gradient.at<float>(i+1,j))
+                        destination.at<float>(i,j) = 255;
+                    else
+                        destination.at<float>(i,j) = 0;
+                } else if (fmod(angle,180) < 157.5) {
+                    if (gradient.at<float>(i,j) >= gradient.at<float>(i+1,j-1) && gradient.at<float>(i,j) >= gradient.at<float>(i-1,j+1))
+                        destination.at<float>(i,j) = 255;
+                    else
+                        destination.at<float>(i,j) = 0;
+                } else if (fmod(angle,180) < 180) {
+                    if (gradient.at<float>(i,j) >= gradient.at<float>(i,j-1) && gradient.at<float>(i,j) >= gradient.at<float>(i,j+1))
+                        destination.at<float>(i,j) = 255;
+                    else
+                        destination.at<float>(i,j) = 0;
+                }
+            } else {
+                destination.at<float>(i,j) = 0;
+            }
+        }
+    }
+}
+
 void on_trackbar( int, void* ) {
     seuil = (float) seuil_slider/seuil_slider_max;
-    seuillage(destinationNorme, destinationNormeSeuil, seuil);
-    cv::imshow("gradientNormeSeuil", destinationNormeSeuil);
+//    seuillage(destinationNorme, destinationNormeSeuil, seuil);
+    seuillage(distMulti, distMultiSeuil, seuil);
+
+    cv::imshow("gradientNormeSeuil", distMultiSeuil);
     
     seuil_hyst = (float) seuil_hyst_slider/seuil_hyst_slider_max;
     seuil_hysteresis(destinationNorme, seuilHaut, seuilHyst, seuil, seuil_hyst);
     cv::imshow("Hysteresis", seuilHyst);
+    
+    affinageMultiDir(distMultiSeuil, MultiThin, distMulti,dirMulti);
+    cv::imshow("affinage", MultiThin);
 }
 
 int convolution (cv::Mat image, cv::Mat derivee, int i, int j, int color) {
@@ -83,7 +163,7 @@ int convolution (cv::Mat image, cv::Mat derivee, int i, int j, int color) {
     }
     temp = abs(temp);
     if (temp > 255) {
-        return 255;
+        return 254;
     }
     else
         return temp;
@@ -117,16 +197,16 @@ cv::Mat directionMulti(cv::Mat dist, cv::Mat& dirColor, cv::Mat a0, cv::Mat a1, 
     
     for (int i=0; i < dist.size().height; ++i) {
         for (int j=0; j < dist.size().width; ++j) {
-            if (dist.at<uchar>(i,j) == 0){
+            if (dist.at<float>(i,j) == 0) {
                 result.at<float>(i,j) = 0;
                 dirColor.at<cv::Vec3b>(i,j) = cv::Vec3b(0,0,0);
-            } else if (dist.at<uchar>(i,j) == a0.at<uchar>(i,j)) {
+            } else if (dist.at<float>(i,j) == a0.at<uchar>(i,j)) {
                 result.at<float>(i,j) = 0;
                 dirColor.at<cv::Vec3b>(i,j) = cv::Vec3b(200,0,0);
-            } else if (dist.at<uchar>(i,j) == a1.at<uchar>(i,j)) {
+            } else if (dist.at<float>(i,j) == a1.at<uchar>(i,j)) {
                 result.at<float>(i,j) = 1 * (M_PI_4);
                 dirColor.at<cv::Vec3b>(i,j) = cv::Vec3b(0,200,0);
-            } else if (dist.at<uchar>(i,j) == a2.at<uchar>(i,j)) {
+            } else if (dist.at<float>(i,j) == a2.at<uchar>(i,j)) {
                 result.at<float>(i,j) = 2 * (M_PI_4);
                 dirColor.at<cv::Vec3b>(i,j) = cv::Vec3b(0,0,200);
             } else {
@@ -177,6 +257,8 @@ int main(int argc, const char * argv[]) {
     destMulti3 = cv::Mat(image.size().height-2,image.size().width-2,image.type());
     
     distMulti = cv::Mat(destMulti0.size(), CV_32F);
+    distMultiSeuil = cv::Mat(distMulti.size(), CV_32F);
+    MultiThin = cv::Mat(distMulti.size().height-2, distMulti.size().width-2, CV_32F);
     dirColorMulti = cv::Mat(distMulti.size(), image.type());
     
     if (!image.data) {
@@ -227,13 +309,21 @@ int main(int argc, const char * argv[]) {
     distMulti = max(distMulti, grayMulti2);
     distMulti = max(distMulti, grayMulti3);
     
+    distMulti.convertTo(distMulti, CV_32F);
+    
     dirMulti = directionMulti(distMulti, dirColorMulti, grayMulti0, grayMulti1, grayMulti2, grayMulti3);
     
+    cv::Scalar mean, stddev;
+    cv::meanStdDev(distMulti, mean, stddev, cv::Mat());
+    printf("%g  %g\n", mean[0], stddev.val[0]/255);
     
-    seuil = 0.09f;
+    seuil = mean.val[0]/255;
+    printf("%d\n",(int) (seuil * 100));
+    seuil_slider = (int) (seuil * 100);
     seuil_hyst = 0.02f;
     
-    seuillage(destinationNorme, destinationNormeSeuil, seuil);
+//    seuillage(destinationNorme, destinationNormeSeuil, seuil);
+    seuillage(distMulti, distMultiSeuil, seuil);
     
     GrisChan[0] = destinationNorme/255;
     GrisChan[1] = destinationNorme/255;
@@ -258,6 +348,8 @@ int main(int argc, const char * argv[]) {
     on_trackbar( seuil_slider, 0 );
     on_trackbar( seuil_hyst_slider, 0 );
     
+
+    
 //    cv::imshow("image", image);
 //
 //    cv::imshow("gradX", destinationX);
@@ -271,6 +363,7 @@ int main(int argc, const char * argv[]) {
     cv::imshow("dist", distMulti);
     cv::imshow("dir", dirMulti);
     cv::imshow("dirColor", dirColorMulti);
+    
 //
 //    cv::imshow("gradientNormeGris", destinationNormeGris);
 //    cv::imshow("gradientDirection", destinationDirection);
